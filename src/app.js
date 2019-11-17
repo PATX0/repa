c14b18445d8d4da227ffb02d4c2d3606c1cc8de1
thyal <thomasdfrivold@gmail.com>
2019-11-17 18:41:27 +0000

    add sequelize setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.use('/users', require('./routes/user.routes'));
 
 app.listen(port, () => console.log(`App is running at port ${port}`));
+
+export default app;
---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export default config =  
+{
+  "development": {
+    "username": process.env.DB_USER_DEV,
+    "password": process.env.DB_PASS_DEV,
+    "database": process.env.DB_NAME_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
