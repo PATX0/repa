3080fa3498c6742f9bd8b1eb6d14c310e75d6fd2
Tiago Soares <43506003+PATX0@users.noreply.github.com>
2020-01-21 20:03:01 +0000

    removed timercode

--- pi/I2C.py
+++ pi/I2C.py
@@ -19,14 +19,7 @@ def readNumber():
     number = bus.read_byte(addr)
     return number
  
-def schedule():
-    if (dt.time() >= datetime.time(15, 47) and flag == 1):
-        if (dt.time() <= datetime.time(15, 48) or flag == 0):
-            IO.output(18, IO.HIGH)
-            time.sleep(1)
-            IO.output(18, IO.LOW)
 while(1):
-    dt = datetime.datetime.now()
     time.sleep(0.5)
     number = readNumber()
     if (number < 2 and flag == 1):
@@ -46,12 +39,3 @@ while(1):
         data["value"] = 2
         data["session_id"] = sessionId
         requests.post('http://178.62.238.173:8080/sessions', data)
-    if (dt.time() >= datetime.time(16, 26) and dt.time() <= datetime.time(16, 28) and flag == 1):
-        #print("ENTERING 1st IF\n")
-        IO.output(18, IO.HIGH)
-        time.sleep(1)
-        IO.output(18, IO.LOW)
-        if (flag == 0):
-            IO.output(18, IO.LOW)
-        else:
-            IO.output(18, IO.HIGH)
